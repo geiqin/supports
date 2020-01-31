33fdb4fb0e4aed0089e8a854afcb61ddbed36b1a
geiqin <40573812+geiqin@users.noreply.github.com>
2020-01-31 21:19:06 +0800

    Create events.go

---
+++ event/events.go
@@ -0,0 +1,31 @@
+package event
+
+import (
+	"github.com/micro/go-micro/broker"
+	"log"
+	"sync"
+)
+
+var pubSub broker.Broker
+var once sync.Once
+
+type EventType string
+
+//消息注册
+func Register(myBroker broker.Broker)  {
+	once.Do(func() {
+		pubSub =myBroker
+		if err := pubSub.Connect(); err != nil {
+			log.Fatal("register broker connect error: %v\n", err)
+		}
+	})
+}
+
+//消息发布事件
+func Publish(eventName EventType , data string) error {
+	msg :=&broker.Message{
+		Body:[]byte(data),
+	}
+	err :=pubSub.Publish(string(eventName),msg)
+	return err
+}
