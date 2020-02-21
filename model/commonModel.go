0de668d3d4570a910150a451d07d248a80babb1d
geiqin <40573812+geiqin@users.noreply.github.com>
2020-02-21 15:07:42 +0800

    Update commonModel.go

--- model/commonModel.go
+++ model/commonModel.go
@@ -4,22 +4,12 @@ import (
 	"time"
 )
 
-//基本Model
-type BaseModel struct {
-	Id        int64 `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time `sql:"index"`
-}
-
-type IntModel struct {
-	Id        int32 `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time `sql:"index"`
-}
-
 type Timestamps struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type AllTimestamps struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
