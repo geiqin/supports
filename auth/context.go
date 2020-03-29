a0fdc59abc0ffb2cbb6ff2525ad36a35aa3e1146
geiqin <40573812+geiqin@users.noreply.github.com>
2020-03-29 17:57:15 +0800

    Update context.go

--- auth/context.go
+++ auth/context.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"context"
 	"github.com/geiqin/supports/helper"
+	"github.com/micro/go-micro/broker"
 )
 
 func StoreContext(storeId int64) context.Context {
@@ -13,4 +14,18 @@ func StoreContext(storeId int64) context.Context {
 func StoreContextByString(storeId string) context.Context {
 	ctx := context.WithValue(context.Background(), "store_id", storeId)
 	return ctx
+}
+
+func StoreContextByBroker(broker broker.Event) context.Context {
+	if broker != nil && broker.Message().Header != nil {
+		storeId := broker.Message().Header["store_id"]
+		if storeId != "" {
+			sid := helper.StringToInt64(storeId)
+			if sid > 0 {
+				ctx := context.WithValue(context.Background(), "store_id", storeId)
+				return ctx
+			}
+		}
+	}
+	return context.Background()
 }
