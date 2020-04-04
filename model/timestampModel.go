b7ae3ed835e33534abb7afc6613e98c0170d4cd7
geiqin <40573812+geiqin@users.noreply.github.com>
2020-04-04 14:05:19 +0800

    Update timestampModel.go

--- model/timestampModel.go
+++ model/timestampModel.go
@@ -1,16 +1,46 @@
 package model
 
 import (
+	"database/sql/driver"
+	"fmt"
+	"strconv"
 	"time"
 )
 
 type Timestamps struct {
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	CreatedAt MyTime `db:"created_at" json:"created_at"`
+	UpdatedAt MyTime `db:"updated_at" json:"updated_at"`
 }
 
 type AllTimestamps struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
+}
+
+type MyTime struct {
+	time.Time
+}
+
+func (t MyTime) MarshalJSON() ([]byte, error) {
+	//格式化秒
+	seconds := t.Unix()
+	return []byte(strconv.FormatInt(seconds, 10)), nil
+}
+
+func (t MyTime) Value() (driver.Value, error) {
+	var zeroTime time.Time
+	if t.Time.UnixNano() == zeroTime.UnixNano() {
+		return nil, nil
+	}
+	return t.Time, nil
+}
+
+func (t *MyTime) Scan(v interface{}) error {
+	value, ok := v.(time.Time)
+	if ok {
+		*t = MyTime{Time: value}
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
 }
