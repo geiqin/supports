c94d6e000b5b64d1548194929db7bca632dd2baf
geiqin <40573812+geiqin@users.noreply.github.com>
2020-04-04 13:28:04 +0800

    Update connection.go

--- database/connection.go
+++ database/connection.go
@@ -60,11 +60,11 @@ func setDbPrefix(prefix string) {
 }
 
 func CreateMysqlDB(cfg *DbConfig) *gorm.DB {
-	//serverAddr := cfg.Host + ":" + cfg.Port
+	serverAddr := cfg.Host + ":" + cfg.Port
 	//timezone := "'Asia/Shanghai'"
 	//connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4", cfg.Username, cfg.Password, serverAddr, cfg.Database)
 	//parseTime=True&    /utf8mb4    Local&time_zone=    Asia%2FShanghai
-	connString := "root:123456@tcp(120.24.243.92:3306)/" + cfg.Database + "?charset=utf8"
+	connString := cfg.Username + ":" + cfg.Password + "@tcp(" + serverAddr + ")/" + cfg.Database + "?charset=utf8&loc=Local&time_zone=%27Asia%2FShanghai%27"
 	//connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local&time_zone=%27Asia%2FShanghai%27", cfg.Username, cfg.Password, serverAddr, cfg.Database)
 	//connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&loc=Local", cfg.Username, cfg.Password, serverAddr, cfg.Database)
 	db, err := gorm.Open("mysql", connString)
