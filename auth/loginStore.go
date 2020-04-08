a5162491940173b3160fd6d9b30409caa87b655c
geiqin <40573812+geiqin@users.noreply.github.com>
2020-04-09 02:27:41 +0800

    1
    1

--- auth/loginStore.go
+++
@@ -1,9 +0,0 @@
-package auth
-
-
-type LoginStore struct {
-	Id   int64
-	Name  string
-	HasLogin bool
-}
-
--- protobuf.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-protoc --proto_path=. --micro_out=. --go_out=. protobuf/*.proto
-
-echo make proto files is ok!
