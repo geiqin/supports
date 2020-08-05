31d6f88b4ce2ab43251cbb3dc43d91265d5a3f37
Tangyonghai <tech@geiqin.com>
2020-08-05 18:19:41 +0800

    111
    1111

--- database/connection.go
+++ database/connection.go
@@ -24,8 +24,8 @@ func CreateMysqlDB(cfg *xconfig.DatabaseInfo) *gorm.DB {
 	//parseTime=True&    /utf8mb4    Local&time_zone=   Asia%2FShanghai
 
 	//当前有效两种
-	//connString := cfg.Username + ":" + cfg.Password + "@tcp(" + serverAddr + ")/" + cfg.Database + "?charset=utf8mb4&loc=Local"
-	connString := cfg.Username + ":" + cfg.Password + "@tcp(" + serverAddr + ")/" + cfg.Database + "?charset=utf8mb4&loc=Local&parseTime=True"
+	connString := cfg.Username + ":" + cfg.Password + "@tcp(" + serverAddr + ")/" + cfg.Database + "?charset=utf8mb4&loc=Local"
+	//connString := cfg.Username + ":" + cfg.Password + "@tcp(" + serverAddr + ")/" + cfg.Database + "?charset=utf8mb4&loc=Local&parseTime=True"
 
 	//connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local&time_zone=%27Asia%2FShanghai%27", cfg.Username, cfg.Password, serverAddr, cfg.Database)
 	//connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&loc=Local", cfg.Username, cfg.Password, serverAddr, cfg.Database)
