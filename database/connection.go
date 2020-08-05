03acce0a9223f50afd1281d29a5a7a0e8f3e7db8
Tangyonghai <tech@geiqin.com>
2020-08-05 18:11:21 +0800

    dd
    dd

--- database/connection.go
+++ database/connection.go
@@ -22,7 +22,11 @@ func CreateMysqlDB(cfg *xconfig.DatabaseInfo) *gorm.DB {
 	//timezone := "'Asia/Shanghai'"
 	//connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4", cfg.Username, cfg.Password, serverAddr, cfg.Database)
 	//parseTime=True&    /utf8mb4    Local&time_zone=   Asia%2FShanghai
-	connString := cfg.Username + ":" + cfg.Password + "@tcp(" + serverAddr + ")/" + cfg.Database + "?charset=utf8mb4&loc=Local"
+
+	//当前有效两种
+	//connString := cfg.Username + ":" + cfg.Password + "@tcp(" + serverAddr + ")/" + cfg.Database + "?charset=utf8mb4&loc=Local"
+	connString := cfg.Username + ":" + cfg.Password + "@tcp(" + serverAddr + ")/" + cfg.Database + "?charset=utf8mb4&loc=Local&parseTime=True"
+
 	//connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local&time_zone=%27Asia%2FShanghai%27", cfg.Username, cfg.Password, serverAddr, cfg.Database)
 	//connString := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&loc=Local", cfg.Username, cfg.Password, serverAddr, cfg.Database)
 	db, err := gorm.Open("mysql", connString)
